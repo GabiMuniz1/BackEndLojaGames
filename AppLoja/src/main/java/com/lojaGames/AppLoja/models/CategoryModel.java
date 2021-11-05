7ef6da65bfc7bebfec62a7a360878cb434b6ac21
Gabi.Muniz <mariliamuniz2001@gmail.com>
2021-11-05 16:04:08 -0300

    @GabiMuniz1 [Task: CREATE FOREIGN KEY]

--- AppLoja/src/main/java/com/lojaGames/AppLoja/models/CategoryModel.java
+++ AppLoja/src/main/java/com/lojaGames/AppLoja/models/CategoryModel.java
@@ -1,12 +1,18 @@
 package com.lojaGames.AppLoja.models;
 
+import java.util.List;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @Entity
 @Table(name = "tb_category")
@@ -23,6 +29,10 @@ public class CategoryModel {
 	@NotBlank
 	@Size(min = 5, max = 500)
 	private String descriptionCategoria;
+
+	@OneToMany(mappedBy = "category", cascade = CascadeType.REMOVE)
+	@JsonIgnoreProperties("category")
+	private List<ProductModel> product;
 
 	public Long getIdCategory() {
 		return idCategory;
