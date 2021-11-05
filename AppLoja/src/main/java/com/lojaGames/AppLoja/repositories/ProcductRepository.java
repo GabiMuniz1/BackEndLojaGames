a4eedec1083defa8fb840e3a176dce8306171e20
Arthur Leandro <arthurdocsgo@gmail.com>
2021-11-05 14:07:54 -0300

    @arthurleand [Task: CREATE ProductModel]

--- AppLoja/src/main/java/com/lojaGames/AppLoja/repositories/ProcductRepository.java
+++ AppLoja/src/main/java/com/lojaGames/AppLoja/repositories/ProcductRepository.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.lojaGames.AppLoja.models.ProductModel;
+
 @Repository
 public interface ProcductRepository extends JpaRepository<ProductModel, Long>{
 
