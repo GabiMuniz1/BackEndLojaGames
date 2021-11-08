18f300502c0f70d0dea36dae5fd7c3f6614e86ea
Arthur Leandro <arthurdocsgo@gmail.com>
2021-11-08 14:30:54 -0300

    @arthurleand [Task: CREATE ProductController AND CategoryController]

---
+++ BackEndFarmacia/src/main/java/com/backendfarmacia/AppFarmacia/controllers/CategoryController.java
@@ -0,0 +1,55 @@
+package com.backendfarmacia.AppFarmacia.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/product")
+@CrossOrigin("*")
+public class CategoryController {
+
+	@Autowired
+	private CategoryRepository repository;
+
+	@GetMapping
+	public ResponseEntity<List<CategoryModel>> findAllCategory() {
+		return ResponseEntity.ok(repository.findAll());
+	}
+
+	@GetMapping("/{id}")
+	public ResponseEntity<CategoryModel> findByIDCategory(@PathVariable Long id) {
+		return repository.findById(id).map(resp -> ResponseEntity.ok(resp)).orElse(ResponseEntity.notFound().build());
+	}
+
+	@GetMapping("/name/{name}")
+	public ResponseEntity<List<CategoryModel>> GetByName(@PathVariable String name) {
+		return ResponseEntity.ok(repository.findAllByNameContainingIgnoreCase(name));
+	}
+
+	@PostMapping
+	public ResponseEntity<CategoryModel> post(@RequestBody CategoryModel category) {
+		return ResponseEntity.status(HttpStatus.CREATED).body(repository.save(category));
+	}
+
+	@PutMapping
+	public ResponseEntity<CategoryModel> put(@RequestBody CategoryModel category) {
+		return ResponseEntity.status(HttpStatus.OK).body(repository.save(category));
+	}
+
+	@DeleteMapping("/{id}")
+	public void delete(@PathVariable Long id) {
+		repository.deleteById(id);
+	}
+}
